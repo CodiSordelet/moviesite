f1e7134b30c1c1b47fdf6f4d527a4f333ee84e04
Zack Downing <zdowning.1@gmail.com>
2017-11-09 12:03:54 -0500

    initial site setup

---
+++ script.js
@@ -0,0 +1,32 @@
+// script.js
+
+    // create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+    // configure our routes
+    scotchApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/main.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/favorites', {
+                templateUrl : 'pages/favorites.html',
+                controller  : 'favoritesController'
+            })
+    });
+
+    // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Search for movies by title or keyword and they will be displayed here!';
+    });
+
+    scotchApp.controller('favoritesController', function($scope) {
+        $scope.message = 'All your favorite movies are displayed here!';
+    });
